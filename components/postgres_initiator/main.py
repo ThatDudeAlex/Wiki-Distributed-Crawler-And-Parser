ddd4b002c90e78afdc6a919566e731d8ab2a6a7b
Alex Nunez <josenunez1692@gmail.com>
2025-06-29 17:04:57 -0400

    Renamed seeder service to rabbitmq_seeder

--- components/postgres_initiator/main.py
+++ components/postgres_initiator/main.py
@@ -2,11 +2,13 @@ import os
 import time
 import sys
 from dotenv import load_dotenv
+from shared.logging_utils import get_logger
 from database.engine import init_db, engine
 from sqlalchemy.exc import OperationalError
 
 # TODO: use a config_service instead of load_dotenv
 # TODO: use a logger here instead of print statements
+logger = get_logger('Postgres_Init')
 load_dotenv()
 
 DATABASE_URL = os.getenv('DATABASE_URL')
@@ -17,24 +19,23 @@ DELAY = 3  # seconds
 def wait_for_db():
     # TODO: this function can be repurposed into a general `wait_for`
     #       and added to my python-utilities package
-    print("Waiting for the database to be ready...")
+    logger.debug("Waiting for the database to be ready...")
     retries = 0
     while retries < MAX_RETRIES:
         try:
             # engine = create_engine(DATABASE_URL)
             with engine.connect():
-                print("\nDatabase is ready!")
+                logger.info("Database is initialized and ready!")
                 return
         except OperationalError as e:
-            print(
+            logger.warning(
                 f"Database not ready yet (attempt {retries + 1}/{MAX_RETRIES}): {str(e)}")
             time.sleep(DELAY)
             retries += 1
-    print("Exceeded maximum retry attempts. Exiting.")
+    logger.error("Exceeded maximum retry attempts. Exiting.")
     sys.exit(1)
 
 
 if __name__ == "__main__":
     wait_for_db()
     init_db()
-    print("Database initialized with tables.")
---
+++ components/rabbitmq_seeder/main.py
@@ -0,0 +1,15 @@
+from shared.config import SEED_URL, ALL_QUEUE_CHANNELS, QueueNames
+from shared.logging_utils import get_logger
+from shared.queue_service import QueueService
+
+
+if __name__ == "__main__":
+    logger = get_logger('RabbitMQ_Seeder')
+
+    queue = QueueService(logger, ALL_QUEUE_CHANNELS)
+
+    queue.publish(QueueNames.CRAWL.value, {
+        "url": SEED_URL,
+        "depth": 0,
+    })
+    logger.info("Seeded Queue with URL: %s", SEED_URL)
