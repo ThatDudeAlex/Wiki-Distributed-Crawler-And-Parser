316d0fcb7a17fbec65d78cf085aa3988739882e4
Alex Nunez <josenunez1692@gmail.com>
2025-07-24 16:30:22 -0400

    Parser: added docstrings to parsing_service

--- components/parser/services/parsing_service.py
+++ components/parser/services/parsing_service.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any
 from shared.rabbitmq.queue_service import QueueService
 from components.parser.core.wiki_content_extractor import PageContentExtractor
 from components.parser.core.wiki_link_extractor import PageLinkExtractor
@@ -9,7 +10,25 @@ from components.parser.core.metrics import PAGES_PARSED_TOTAL, LINKS_EXTRACTED_T
 
 
 class ParsingService:
-    def __init__(self, configs, queue_service: QueueService, logger: logging.Logger):
+    """
+    Orchestrates the parsing workflow for a given task
+
+    This includes:
+        - Loading and decompressing HTML content from file
+        - Extracting structured page content
+        - Extracting hyperlinks to schedule
+        - Publishing parsed content and links to the appropriate queues
+    """
+
+    def __init__(self, configs: dict[str, Any], queue_service: QueueService, logger: logging.Logger):
+        """
+        Initializes the ParsingService
+
+        Args:
+            configs (dict): Configuration dictionary for extractors
+            queue_service (QueueService): RabbitMQ interface for publishing results
+            logger (logging.Logger): Logger instance
+        """
         self._queue_service = queue_service
         self._logger = logger
         self.content_extractor = PageContentExtractor(configs, logger)
@@ -17,6 +36,18 @@ class ParsingService:
         self._publisher = PublishingService(self._queue_service, logger)
 
     def run(self, task: ParsingTask):
+        """
+        Executes the full parsing pipeline for a single task.
+
+        Steps:
+            1. Load compressed HTML from disk
+            2. Extract structured content and links
+            3. Publish parsed content to be saved
+            4. Publish extracted links for scheduling
+
+        Args:
+            task (ParsingTask): The parsing task containing URL, depth, and file path.
+        """
         url = task.url
         depth = task.depth
         filepath = task.compressed_filepath
@@ -28,9 +59,7 @@ class ParsingService:
                 html_content = load_compressed_html(filepath, self._logger)
 
                 if html_content is None:
-                    self._logger.error(
-                        "Skipping Parsing Task - Html content did not load"
-                    )
+                    self._logger.error("Skipping Parsing Task - HTML content could not be loaded")
                     return
 
             with STAGE_DURATION_SECONDS.labels("extract_content").time():
@@ -50,11 +79,11 @@ class ParsingService:
                 self._logger.info('STAGE 5: Publish Process Links')
                 self._publisher.publish_process_links_task(page_links)
 
-            self._logger.info('Parsing Task Successfully Completed!')
+            self._logger.info('Parsing Task Completed for URL: %s', url)
 
         except Exception:
+            self._logger.exception("Unexpected error during parsing task for %s", url)
             # TODO: Consider sending failed parsing task to DLQ
-            self._logger.exception("Unexpected error during parsing task")
 
         finally:
             PAGES_PARSED_TOTAL.inc()
