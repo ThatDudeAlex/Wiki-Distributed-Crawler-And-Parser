055bc21b5d84f8273b13a10caa3251e455f6ac9b
Alex Nunez <josenunez1692@gmail.com>
2025-07-25 05:28:47 -0400

    Dispatcher: added docstrings to the publisher

--- components/dispatcher/main.py
+++ components/dispatcher/main.py
@@ -6,16 +6,29 @@ from shared.configs.config_loader import component_config_loader
 
 COMPONENT_NAME = "dispatcher"
 
-def run():
+def run() -> None:
+    """
+    Dispatcher Component Entrypoint
+
+    Responsible for:
+        - Loading configurations
+        - Initializing logger and RabbitMQ queue service
+        - Running the Dispatcher service
+    """
     configs = component_config_loader(COMPONENT_NAME)
+    
+    # TODO: look into whether using a pydantic class using dict.get() with a default would
+    #       be a better way to access the configuration values
     logger = get_logger(
-        configs['logging']['logger_name'], configs['logging']['log_level']
+        configs['logging']['logger_name'], 
+        configs['logging']['log_level']
     )
 
     queue_service = QueueService(logger, DispatcherQueueChannels.get_values())
 
     dispatcher = Dispatcher(configs, queue_service, logger)
 
+    logger.info("Starting Dispatcher Component...")
     dispatcher.run()
 
 
--- components/dispatcher/services/publisher.py
+++ components/dispatcher/services/publisher.py
@@ -5,23 +5,49 @@ from shared.rabbitmq.schemas.crawling import CrawlTask
 from shared.rabbitmq.queue_service import QueueService
 
 
-# TODO: I temporarily commented out logging to test how it affects performace
-#       put them back when needed
-
 class PublishingService:
+    """
+    Service for publishing tasks to RabbitMQ queues
+
+    Publishes:
+        - CrawlTask messages to the 'urls_to_crawl' queue
+    """
+
     def __init__(self, queue_service: QueueService, logger: logging.Logger):
+        """
+        Initialize the publishing service
+
+        Args:
+            queue_service (QueueService): Service for publishing messages to RabbitMQ
+            logger (logging.Logger): Logger instance
+        """
         self._queue_service = queue_service
         self._logger = logger
 
-    def publish_crawl_tasks(self, links_to_crawl: List[CrawlTask]):
-        link_count = 0
-        # self._logger.info("Publishing links to crawl: %s", links_to_crawl)
 
-        for link in links_to_crawl:
-            message = link
+    def publish_crawl_tasks(self, crawl_tasks: List[CrawlTask]) -> None:
+        """
+        Publish a list of CrawlTask messages to the 'urls_to_crawl' queue
 
-            self._queue_service.publish(
-                SchedulerQueueChannels.URLS_TO_CRAWL.value, message.model_dump_json())
-            link_count += 1
+        Each task is serialized to JSON and sent via the queue service.
+        Logs the total number of tasks published.
 
-        # self._logger.info("Published: %s Links To Crawl", link_count)
+        Args:
+            crawl_tasks (List[CrawlTask]): List of CrawlTask Pydantic models to publish
+        """
+        successful = 0
+
+        for task in crawl_tasks:
+            try:
+                self._queue_service.publish(
+                    SchedulerQueueChannels.URLS_TO_CRAWL.value, 
+                    task.model_dump_json()
+                )
+                self._logger.debug("Published Crawl Task: %s", task)
+                successful += 1
+
+            except Exception:
+                self._logger.exception("Failed to publish crawl task")
+
+        self._logger.info("Successfully published %d out of %d crawl tasks", successful, len(crawl_tasks))
+
